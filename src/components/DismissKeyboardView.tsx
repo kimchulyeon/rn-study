36beec02c395f4219b83f51284f6faa9cde719f0
kim chulyeon <guinness987@gmail.com>
2022-11-03 14:53:19 +0900

    fix: DismissKeyboardView 클릭 이벤트 제거

--- src/components/DismissKeyboardView.tsx
+++ src/components/DismissKeyboardView.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {TouchableWithoutFeedback, Keyboard, StyleProp, ViewStyle} from 'react-native';
+import {TouchableWithoutFeedback, StyleProp, ViewStyle} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const DismissKeyboardView: React.FC<{
   children: React.ReactNode;
   style?: StyleProp<ViewStyle>;
 }> = ({children, ...props}) => (
-  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+  <TouchableWithoutFeedback accessible={false}>
     <KeyboardAwareScrollView {...props} style={props.style}>
       {children}
     </KeyboardAwareScrollView>
